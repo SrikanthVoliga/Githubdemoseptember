db001056be64f6e0f5d939b57c45582294be9a9d
Home <Home@DESKTOP-G57JOSA>
2022-09-22 17:35:36 +0530

    New Project

---
+++ Githubdemo/src/com/demogithub/Moveuser.java
@@ -0,0 +1,11 @@
+package com.demogithub;
+
+public class Moveuser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to github successfully");
+
+	}
+
+}
